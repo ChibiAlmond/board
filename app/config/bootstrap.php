4798ba6d3125b1f691e35c68a41cb4c05a76cd40
ChibiAlmond <a.sato@klab.com>
2013-11-22 16:09:56 +0800

    Revised boostrap.php: required validation_helper.php

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -7,6 +7,7 @@ require_once APP_DIR.'app_exception.php';
 
 // helpers
 require_once HELPERS_DIR.'html_helper.php';
+require_once HELPERS_DIR.'validation_helper.php';
 require_once CONFIG_DIR.'router.php';
 require_once CONFIG_DIR.'database.php';
 
