4b1a79633cb6d12eb45317937347c02b7231fed0
ChibiAlmond <a.sato@klab.com>
2013-11-22 16:23:07 +0800

    modified switch case method in thread_controller.php by adding try catch for validation

--- app/controllers/thread_controller.php
+++ app/controllers/thread_controller.php
@@ -11,22 +11,28 @@ class ThreadController extends AppController
     {
         $thread = Thread::get(Param::get('thread_id'));
         $comment = new Comment;
-        $page = Param::get('page_next');
+        $page = Param::get('page_next', 'write');
 
 		switch ($page) {
+		case 'write':
+        break;
         case 'write_end':
             $comment->username = Param::get('username');
             $comment->body = Param::get('body');
-            $thread->write($comment);
+			try {
+                $thread->write($comment);
+			} catch (ValidationErrorException $e) {
+			    $page = 'write';
+			}
             break;
         default:
             throw new NotFoundException("{$page} is not found");
             break;
         }
 		
-    $this->set(get_defined_vars());
-    $this->render($page);
-}
+        $this->set(get_defined_vars());
+        $this->render($page);
+    }
 
 
     public function view()
