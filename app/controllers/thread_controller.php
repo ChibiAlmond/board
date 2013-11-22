204b1beee47313406d52e0faaa33fa05a3d1c6fe
ChibiAlmond <a.sato@klab.com>
2013-11-22 15:22:44 +0800

    added view() to thread_controller.php

--- app/controllers/thread_controller.php
+++ app/controllers/thread_controller.php
@@ -4,6 +4,15 @@ class ThreadController extends AppController
     public function view()
     {
     $thread = Thread::get(Param::get(’thread_id’));
+    
+	$this->set(get_defined_vars());
+    }
+	
+    public function view()
+    {
+    $thread = Thread::get(Param::get(’thread_id’));
+    $comments = $thread->getComments();
+	
     $this->set(get_defined_vars());
     }
 }
