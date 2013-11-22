16144913a17f20478aca64bb89c926fb8aad2c72
ChibiAlmond <a.sato@klab.com>
2013-11-22 16:31:33 +0800

    added a link to thread/create in index.php

--- app/views/thread/index.php
+++ app/views/thread/index.php
@@ -2,9 +2,10 @@
 <ul>
     <?php foreach ($threads as $v): ?>
     <li>
-	    <a href="<?php eh(url(’thread/view’, array(’thread_id’ => $v->id))) ?>">
+	    <a href="<?php eh(url('thread/view', array('thread_id' => $v->id))) ?>">
         <?php eh($v->title) ?>
         </a>
     </li>
     <?php endforeach ?>
 </ul>
+<a class="btn btn-large btn-primary" href="<?php eh(url('thread/create')) ?>">Create</a>
