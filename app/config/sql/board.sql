8e0b7038e87f6faf39549c3365057ffbcbd9df9f
Almond <coffeevanillaicecream@gmail.com>
2013-11-24 12:56:25 -0800

    added users to table in board.sql

--- app/config/sql/board.sql
+++ app/config/sql/board.sql
@@ -1,30 +1,85 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
 --
--- Create database
---
-CREATE DATABASE IF NOT EXISTS board;
-GRANT SELECT, INSERT, UPDATE, DELETE ON board.* TO board_root@localhost IDENTIFIED BY 'board_root';
-FLUSH PRIVILEGES;
---
--- Create tables
---
-USE board;
-CREATE TABLE IF NOT EXISTS thread (
-id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-title VARCHAR(255) NOT NULL,
-created DATETIME NOT NULL,
-PRIMARY KEY (id)
-)ENGINE=InnoDB;
-CREATE TABLE IF NOT EXISTS comment (
-id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-thread_id INT UNSIGNED NOT NULL,
-username VARCHAR(255) NOT NULL,
-body TEXT NOT NULL,
-created DATETIME NOT NULL,
-PRIMARY KEY (id),
-INDEX (thread_id, created)
-)ENGINE=InnoDB;
+-- Host: 127.0.0.1
+-- Generation Time: Nov 24, 2013 at 09:45 PM
+-- Server version: 5.5.32
+-- PHP Version: 5.4.19
 
-INSERT INTO thread SET title='Hello', created=NOW();
-INSERT INTO comment SET thread_id=1, username='sakana-san', body='I am hungry', created=NOW();
-INSERT INTO thread SET title='Thread2', created=NOW();
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `board`
+--
+CREATE DATABASE IF NOT EXISTS `board` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `board`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `thread_id` int(10) unsigned NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `thread_id` (`thread_id`,`created`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`id`, `thread_id`, `username`, `body`, `created`) VALUES
+(1, 1, 'sakana-san', 'I am hungry', '2013-11-21 11:02:23'),
+(2, 1, 'sakana-san', 'I am hungry', '2013-11-21 11:06:15'),;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `thread`
+--
+
+CREATE TABLE IF NOT EXISTS `thread` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `thread`
+--
+
+INSERT INTO `thread` (`id`, `title`, `created`) VALUES
+(1, 'Hello', '2013-11-21 11:02:23'),
+(2, 'Hello', '2013-11-21 11:06:15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
